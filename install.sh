c229b0b54a56c42ec91bcafe3a46d45ee6088f43
Tho Nguyen Duc <ndtho8205@gmail.com>
2020-04-09 19:05:51 +0900

    Add ssh config

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/usr/bin/env bash
 
-export DOTFILES="$(cd "$(dirname "$0")"; pwd -P)"
+export DOTFILES="$(
+  cd "$(dirname "$0")"
+  pwd -P
+)"
 
 set -e
 echo ''
 
-if test -t 1
-then
-  ncolors=$(which tput > /dev/null && tput colors)
-  if test -n "$ncolors" && test "$ncolors" -ge 8
-  then
+if test -t 1; then
+  ncolors=$(which tput >/dev/null && tput colors)
+  if test -n "$ncolors" && test "$ncolors" -ge 8; then
     termcols=$(tput cols)
     bold="$(tput bold)"
     underline="$(tput smul)"
@@ -26,60 +27,63 @@ then
   fi
 fi
 
-info () {
+info() {
   printf "\r  [ ${blue}..${normal} ] $1\n"
 }
 
-ask () {
+ask() {
   printf "\r  [ ${yellow}??${normal} ] $1\n"
 }
 
-success () {
+success() {
   printf "\r\033[2K  [ ${green}OK${normal} ] $1\n"
 }
 
-fail () {
+fail() {
   printf "\r\033[2K  [${red}FAIL${normal}] $1\n"
   echo ''
   exit
 }
 
-make_link () {
+make_link() {
   local src=$1 dst=$2
 
   local overwrite= backup= skip=
   local action=
 
-  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]
-  then
+  if [ -f "$dst" -o -d "$dst" -o -L "$dst" ]; then
 
-    if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]
-    then
+    if [ "$overwrite_all" == "false" ] && [ "$backup_all" == "false" ] && [ "$skip_all" == "false" ]; then
       local currentSrc="$(readlink $dst)"
 
-      if [ "$currentSrc" == "$src" ]
-      then
-        skip=true;
+      if [ "$currentSrc" == "$src" ]; then
+        skip=true
       else
         ask "File already exists: $dst ($(basename "$src")), what do you want to do?\n\
         [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all?"
         read -n 1 action
 
         case "$action" in
-          o )
-            overwrite=true;;
-          O )
-            overwrite_all=true;;
-          b )
-            backup=true;;
-          B )
-            backup_all=true;;
-          s )
-            skip=true;;
-          S )
-            skip_all=true;;
-          * )
-            ;;
+        o)
+          overwrite=true
+          ;;
+        O)
+          overwrite_all=true
+          ;;
+        b)
+          backup=true
+          ;;
+        B)
+          backup_all=true
+          ;;
+        s)
+          skip=true
+          ;;
+        S)
+          skip_all=true
+          ;;
+        *) ;;
+
         esac
       fi
     fi
@@ -88,43 +92,37 @@ make_link () {
     backup=${backup:-$backup_all}
     skip=${skip:-$skip_all}
 
-    if [ "$overwrite" == "true" ]
-    then
+    if [ "$overwrite" == "true" ]; then
       rm -rf "$dst"
       success "removed $dst"
     fi
 
-    if [ "$backup" == "true" ]
-    then
+    if [ "$backup" == "true" ]; then
       mv "$dst" "${dst}.backup"
       success "moved $dst to ${dst}.backup"
     fi
 
-    if [ "$skip" == "true" ]
-    then
+    if [ "$skip" == "true" ]; then
       success "skipped $src"
     fi
   fi
 
-  if [ "$skip" != "true" ]  # "false" or empty
-  then
+  if [ "$skip" != "true" ]; then # "false" or empty
     ln -s "$1" "$2"
     success "linked $1 to $2"
   fi
 }
 
-install_dotfiles () {
+install_dotfiles() {
   info 'Installing dotfiles'
 
   local overwrite_all=false backup_all=false skip_all=false
 
-  if [[ ! ( -f "$HOME/.dotfiles" || -d "$HOME/.dotfiles" ) || ( "$DOTFILES" != "$HOME/.dotfiles" ) ]]
-  then
+  if [[ ! (-f "$HOME/.dotfiles" || -d "$HOME/.dotfiles") || ("$DOTFILES" != "$HOME/.dotfiles") ]]; then
     make_link "$DOTFILES" "$HOME/.dotfiles"
   fi
 
-  for src in $(find -H "$DOTFILES" -maxdepth 2 -name '*.symlink' -not -path '*.git*')
-  do
+  for src in $(find -H "$DOTFILES" -maxdepth 2 -name '*.symlink' -not -path '*.git*'); do
     dst="$HOME/.$(basename "${src%.*}")"
     make_link "$src" "$dst"
   done
