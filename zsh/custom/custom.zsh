2e920d501d9b110319a910b4bc53e536967ea2b1
Tho Nguyen Duc <ndtho8205@gmail.com>
2020-10-05 04:34:28 +0900

    Add NordVPN, Calibre, Obsidian to dev_env.

--- zsh/custom/custom.zsh
+++ zsh/custom/custom.zsh
@@ -1,4 +1,5 @@
 # export PATH="/usr/local/cuda-10.0/bin:${PATH}"
 # export LD_LIBRARY_PATH="/usr/local/cuda-10.0/lib64:/usr/local/cuda/extras/CUPTI/lib64:${LD_LIBRARY_PATH}"
 
-export PATH="$PATH:/snap/bin"
+export PATH="$HOME/.cargo/bin:$PATH"
+export PATH="$HOME/.poetry/bin:$PATH"
